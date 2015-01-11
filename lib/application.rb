057026e1ef7e6c591d4f5e751a767b251c9d154c
Neha <webonise@weboniselab.(none)>
2015-01-11 10:36:54 +0530

    correction in requiring files

--- lib/application.rb
+++ lib/application.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby -wKU
 # encoding: UTF-8
-require File.dirname(__FILE__) + '/restaurant/errors.rb'
-require File.dirname(__FILE__) + '/restaurant/restaurant.rb'
-require File.dirname(__FILE__) + '/restaurant/data_input.rb'
+require File.dirname(__FILE__) + '/search/errors.rb'
+require File.dirname(__FILE__) + '/search/restaurant.rb'
+require File.dirname(__FILE__) + '/search/data_input.rb'
