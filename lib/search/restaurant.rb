83ee855139d34a3368ebf553ecd5b770d4a9c888
Neha <webonise@weboniselab.(none)>
2015-01-11 16:05:35 +0530

    working on algo for search implementation

--- lib/search/restaurant.rb
+++ lib/search/restaurant.rb
@@ -8,6 +8,7 @@ module Search
 
     def initialize(input)
       @restaurant_menus = input
+      @menu_items = {}
     end
 
     def my_best_deal(items)
@@ -17,6 +18,15 @@ module Search
     end
 
     private
+
+    def search_items(searched_items)
+      deals = {}
+      @menu_items.each_with_index do |restaurant_id, menu|
+        menu.each_with_index do |price, items|
+          deals.merge!({price => items}) if matching_items?(items, searched_items)
+        end
+      end
+    end
 
     def read_csv
       # IO.foreach doesn't read the entire file into memory at once, which is good since a standard FasterCSV.parse on this file can take an hour or more
@@ -31,27 +41,21 @@ module Search
       row[0].to_i > 0 && row[1].to_f.round(2) >= 0.00 && row[2..-1].size > 0
     end
 
+    def matching_items?(menu_items, searched_items)
+      (menu_items & searched_items).sort == searched_items.sort
+    end
+
     def	store(row)
       restaurant_id, price, items= row[0].strip!.to_i, row[1].strip!.to_f.round(2), row[2..-1]
       items = items.reject { |item|  item.strip! == "" }
-      @restaurant_menus[restaurant_id] = {price => items}
+      @menu_items[restaurant_id] = {price => items}
     end
 
     def available_menu_item?(items)
-      available_items = @restaurant_menus.each_with_index do |restaurant_id, menu|
+      available_items = @menu_items.each_with_index do |restaurant_id, menu|
         menu.values.join(', ').uniq!
       end
       (available_items & items).length == items.length
-    end
-# it returns the item_id of a given item
-# if item is new its added to items list and its id is returned
-    def item(menu_item)
-      if @items.include?(menu_item)
-        @items.index(menu_item)
-      else
-        @items << menu_item
-        @items.length - 1
-      end
     end
   end
 end
